1556e0a64d8f7e6fc92ad83d21b73ec1d43ec3bc
Gurveer <gurveer2202@gmail.com>
2022-11-25 15:38:48 -0600

    form validate

---
+++ READme.md
@@ -0,0 +1,10 @@
+## Contacts-added
+
+A
+
+### Demo
+
+Click [here]( https://gurveerkaur1.github.io/Shape-factory/) to check.
+
+
+
--- assets/script/index.js
+++ assets/script/index.js
@@ -9,47 +9,77 @@ const number = select('.count')
 const child = select('.child')
 let count = 0
 let arr = [];
-onEvent('click', button, function(event){
-    event.preventDefault();
-      count+=1;
-    const div = document.createElement('div');
+
+const firstName = select('.first');
+const para = select('p')
+const form = select('form')
+const emailRegex = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/;
+
+//Form validation
+function validateFormInput () {
+    if(firstName.value !== '') {
+        const contactValues = firstName.value.split(', ');
+        let message = '';
+        let valid = true;
     
-      div.classList.add("shapes");
-      let firstName = select('.first');
-      
+        if(contactValues.length === 3) {
+            // validate email
+            if(!emailRegex.test(contactValues[2])) {
+                para.innerHTML= 'A valid Email is required';
+                valid = false;
+            }
+
+        } else {
+         para.innerHTML= 'A valid email is required';
+            valid = false;
+        }
+        
+        if (!valid) {
+            para.innerHTML = `Full Name, City and Email are required`;
+        } 
+        else {
+          para.innerHTML = 'Form submitted'
+          message+='';
+
+         
+         count+=1;
+         const div = document.createElement('div');
+       
+         div.classList.add("shapes");
+         let firstName = select('.first');
+         child.appendChild(div);
+         let arr2 =firstName.value;
+         let arr3 = arr2.split(',');
+         let newContact = new Contact(arr3[0], arr3[1], arr3[2]).getInfo();
+         arr.push(newContact)
+         const para1 = document.createElement('p');
    
-    console.log()
-    child.appendChild(div);
-    
-    let arr2 =firstName.value;
-   
+         para1.innerText = newContact
+         div.appendChild(para1)
+         number.innerText = arr.length;
+        
+         onEvent('click', div, function(event){
+        event.preventDefault();
+        div.remove();
+        number.innerText = arr.length;
+    })
+}
+} else {
+        para.innerHTML = `Please enter Contact Information`;
+    }
+} 
 
-let arr3 = arr2.split(',');
-let newContact = new Contact(arr3[0], arr3[1], arr3[2]).getInfo();
-arr.push(newContact)
-const para1 = document.createElement('p');
-const para2 = document.createElement('p');
-const para3 = document.createElement('p');
-    para1.innerText = 'ji'; 
-    para2.innerText = arr[1];
-    para3.innerText = arr[2];
-    div.appendChild(para1)
-    div.appendChild(para2)
-    div.appendChild(para3)
-//div.innerText = newContact;
-console.log(newContact);
-
-
-
-onEvent('click', div, function(event){
+// validate form when add button is clicked and add div
+onEvent('click', button, function (event) {
     event.preventDefault();
-    div.remove();
-})
-
-
+    validateFormInput();
+    firstName.value = '';
+    message+= '';
+    
 });
 
-onEvent('click', button, function(event){
-    event.preventDefault();
-    number.innerText = count;
-})
+onEvent('load', window, () => {
+    div.innerHTML = '';
+    form.reset();
+});
+
